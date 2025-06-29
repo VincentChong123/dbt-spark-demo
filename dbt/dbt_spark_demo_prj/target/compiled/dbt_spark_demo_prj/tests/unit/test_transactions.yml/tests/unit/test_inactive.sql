cd755a000d3c912824ea5fc05312088687e106d5
Vincent <ws.chong.sg@gmail.com>
2025-06-29 23:27:57 +0800

    add index.html

---
+++ dbt/dbt_spark_demo_prj/target/compiled/dbt_spark_demo_prj/tests/unit/test_transactions.yml/tests/unit/test_inactive.sql
@@ -0,0 +1,45 @@
+
+
+WITH  __dbt__cte__raw__transactions as (
+
+-- Fixture for raw__transactions
+select cast(2 as bigint)
+ as id, cast(100 as bigint)
+ as amount, cast('SGD' as string)
+ as currency, cast('INACTIVE' as string)
+ as status
+), base AS (
+    SELECT
+        id,
+        amount,
+        currency,
+        status
+    FROM __dbt__cte__raw__transactions
+),
+
+filtered AS (
+    SELECT *
+    FROM base
+    WHERE status = 'ACTIVE'
+),
+
+final AS (
+    SELECT
+        id,
+        amount,
+        currency,
+        CASE
+            WHEN currency = 'USD' THEN amount
+            WHEN currency = 'SGD' THEN amount * 0.74
+            -- ELSE NULL
+        END AS amount_usd,
+        CASE
+            WHEN currency = 'USD' THEN 'USD_BRANCH'
+            WHEN currency = 'SGD' THEN 'SGD_BRANCH'
+            ELSE 'UNKNOWN_BRANCH'
+        END AS branch_marker
+    FROM filtered
+)
+
+SELECT *
+FROM final
--- demo/generate-documentation.sh
+++ demo/generate-documentation.sh
@@ -1,4 +1,5 @@
 quarto render ./demo/dbt-spark-demo.ipynb
+cp ./demo/dbt-spark-demo.html ./index.html
 
 dbt docs generate --project-dir=/home/vin/01-prj/stripe/dbt-spark-demo/dbt/dbt_spark_demo_prj --profiles-dir=/home/vin/01-prj/stripe/dbt-spark-demo/demo
 
