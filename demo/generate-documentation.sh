7ceb96933d10921ce3ae2b4b72f695e14db784a9
Vincent <ws.chong.sg@gmail.com>
2025-06-29 23:25:15 +0800

    udpate documentation

--- demo/generate-documentation.sh
+++ demo/generate-documentation.sh
@@ -1,2 +1,5 @@
-!quarto render ./dbt-spark-demo.ipynb
+quarto render ./demo/dbt-spark-demo.ipynb
+
+dbt docs generate --project-dir=/home/vin/01-prj/stripe/dbt-spark-demo/dbt/dbt_spark_demo_prj --profiles-dir=/home/vin/01-prj/stripe/dbt-spark-demo/demo
+
 dbt docs serve --project-dir=/home/vin/01-prj/stripe/dbt-spark-demo/dbt/dbt_spark_demo_prj --profiles-dir=/home/vin/01-prj/stripe/dbt-spark-demo/demo --port 8089
--- docker-common.sh
+++ docker-common.sh
@@ -6,5 +6,4 @@ cd ../dbt-spark;    docker compose up -d
 docker exec dbt-dbt-1 bash -c "cd /usr/app/demo/dbt_spark_demo_prj && . ./setup.sh"
 
 # jupyter nbconvert --to notebook --execute --inplace a.ipynb
-docker exec -it dbt-dbt-1 jupyter notebook --NotebookApp.token='' --NotebookApp.password='' --NotebookApp.disable_check_xsrf=True --allow-root --ip=0.0.0.0 --port=8888 --no-browser
-cd ..
+docker exec -it dbt-dbt-1 bash -c "cd /usr/app/ && jupyter notebook --NotebookApp.token='' --NotebookApp.password='' --NotebookApp.disable_check_xsrf=True --allow-root --ip=0.0.0.0 --port=8888 --no-browser"
