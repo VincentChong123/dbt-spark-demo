54d937ebd28e78322e637cfd34850227fd1b83f8
Vincent <ws.chong.sg@gmail.com>
2025-06-30 18:00:54 +0800

    remove docker exec -it

--- run-demo.sh
+++ run-demo.sh
@@ -10,8 +10,13 @@ rm demo/results/*.txt
 
 # to show progress of notebook "run all" cell execution status in terminal, 
 # without opening notebook session in web browswer
-docker exec -it dbt-dbt-1 bash -c "uv pip install --system papermill" 
-docker exec -it dbt-dbt-1 bash -c "papermill /usr/app/demo/dbt-spark-demo.ipynb /usr/app/demo/dbt-spark-demo.ipynb"
+echo -------- start docker exec
+# docker exec -it dbt-dbt-1 bash -c "uv pip install --system papermill" 
+# docker exec -it dbt-dbt-1 bash -c "papermill /usr/app/demo/dbt-spark-demo.ipynb /usr/app/demo/dbt-spark-demo.ipynb"
 
-# jupyter nbconvert --to notebook --execute --inplace a.ipynb
+docker exec dbt-dbt-1 bash -c "uv pip install --system papermill" 
+docker exec dbt-dbt-1 bash -c "papermill /usr/app/demo/dbt-spark-demo.ipynb /usr/app/demo/dbt-spark-demo.ipynb"
+echo -------- end docker exec
+
+# jupyter nbconvert --to notebook --execute /usr/app/demo/dbt-spark-demo.ipynb
 # docker exec -it dbt-dbt-1 bash -c "cd /usr/app/ && jupyter notebook --NotebookApp.token='' --NotebookApp.password='' --NotebookApp.disable_check_xsrf=True --allow-root --ip=0.0.0.0 --port=8888 --no-browser"
